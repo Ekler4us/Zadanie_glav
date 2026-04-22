449ea7012b7c862b9d3bb59fc467dcd73a8b8c53
Bogdan <bogalezav@gmail.com>
2026-04-23 01:10:09 +0700

    удалил папку

--- gl8/includes/hfcal.h
+++
@@ -1,6 +0,0 @@
-#ifndef HFCAL_H
-#define HFCAL_H
-
-void display_calories(float weight, float distance, float coeff);
-
-#endif
--- gl8/includes/hfcall.h
+++
@@ -1,6 +0,0 @@
-#ifndef HFCALL_H
-#define HFCALL_H
-
-void display_calories(float weight, float distance, float coeff);
-
-#endif
